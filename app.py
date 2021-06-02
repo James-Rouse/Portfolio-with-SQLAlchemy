2badcd7e79f4d46fce3e8eb2a64fa943bb312b42
James-Rouse <woootlooops@gmail.com>
2021-06-02 16:06:05 -0400

    Add layout.html

--- app.py
+++ app.py
@@ -1,28 +1,33 @@
-from flask import render_template, url_for, request
+from flask import render_template, url_for, request, redirect
 from models import db, Project, app
 
 @app.route("/")
 def portolio_index():
+    """Homepage/index."""
     portfolio = Project.query.all()
     return render_template("index.html", portfolio=portfolio)
 
 @app.route("/project/<id>")
 def project_details():
-    pass
+    """Display details of a project."""
+    return render_template("detail.html")
 
 @app.route("/project/new")
 def new_project():
+    """Add a new project."""
     pass
 
 @app.route("/project/<id>/edit")
 def edit_project():
+    """Edit a project."""
     pass
 
 @app.route("/delete/<id>")
 def delete_project(id):
+    """Delete a project."""
     pass
 
 
 if __name__ == "__main__":
     db.create_all()
-    app.run(debug=True, port=8000, host="0.0.0.0")
+    app.run(debug=True, port=8000, host="127.0.0.1")
