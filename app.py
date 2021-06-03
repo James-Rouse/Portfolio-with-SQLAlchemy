3d17564cbf745ed7e178b8c8fd1e685d5d315c51
James-Rouse <woootlooops@gmail.com>
2021-06-02 21:45:04 -0400

    Add 404 functionality

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def skills_section():
 @app.route("/project/<id>")
 def project_details(id):
     """Display details of a project."""
-    project = Project.query.get(id)
+    project = Project.query.get_or_404(id)
     return render_template("detail.html", project=project)
 
 @app.route("/project/new", methods=['GET', 'POST'])
@@ -57,7 +57,7 @@ def new_project():
 @app.route("/project/<id>/edit", methods=['GET', 'POST'])
 def edit_project(id):
     """Edit a project."""
-    project = Project.query.get(id)
+    project = Project.query.get_or_404(id)
     if request.form:
         split_month_and_year = request.form["date"].split("-")
         year = int(split_month_and_year[0])
@@ -77,10 +77,14 @@ def edit_project(id):
 @app.route("/projects/<id>/delete", methods=['GET', 'POST'])
 def delete_project(id):
     """Delete a project."""
-    project = Project.query.get(id)
+    project = Project.query.get_or_404(id)
     db.session.delete(project)
     db.session.commit()
     return redirect(url_for("portfolio_index"))
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template("404.html", msg=error), 404
 
 
 if __name__ == "__main__":
